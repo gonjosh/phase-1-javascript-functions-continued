5c6f09a2c247c653b5f43de8fba705cb9a661cb3
Joshua Gonzalez <gonjosh@gmail.com>
2022-05-19 22:08:19 -0400

    commit tree 907e2e94148070f82644bcb428b9c25d7c77c59f

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // code your solution here
+function saturdayFun(activity = "roller-skate") {
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun('play sports');
+
+function mondayWork (activity = "go to the office") {
+    return(`This Monday, I will ${activity}.`);
+}
+mondayWork("go to a meeting");
+
+function wrapAdjective(action) {
+    const part1 = 'You are';
+    const part2 = 'a hard worker';
+    const part3 = 'a dedicated programmer'
+    if (action === "*"){
+        return () => `${part1} *${part2}*!`;
+    }
+    if (action === "||"){
+        return () => `${part1} ||${part3}||!`;
+    }
+}
+
+
